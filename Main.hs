1bd1a2f389a21a14ca68a1da72e88d34b6d8059d
Nick Smallbone <nicsma@chalmers.se>
2011-03-21 13:46:01 +0000

    cannot believe he has just written a little spinning progress bar

--- Main.hs
+++ Main.hs
@@ -3,13 +3,29 @@ module Main where
 
 import Lexer
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import System.IO
 
-lastAndLength :: [a] -> (a, Int)
-lastAndLength = go 0
-  where go !n [x] = (x, n+1)
-        go !n (_:xs) = go (n+1) xs
+data Progress a = Tick (Progress a) | Done a
+
+lastAndLength :: [a] -> Progress (a, Int)
+lastAndLength = go 0 1
+  where go !n 250000 xs = Tick (go n 1 xs)
+        go !n t [x] = Done (x, n+1)
+        go !n t (_:xs) = go (n+1) (t+1) xs
+
+progress :: String -> Progress a -> IO a
+progress msg x = putStr msg >> go 0 x
+  where go n x = do
+          putStr (spinny n) >> hFlush stdout
+          case x of
+            Tick x -> go ((n+1) `mod` 4) x
+            Done x -> putStrLn "." >> return x
+        spinny 0 = ".-\08"
+        spinny 1 = "\\\08"
+        spinny 2 = "|\08"
+        spinny 3 = "/\08"
 
 main = do
   tokens <- fmap scan BSL.getContents
-  let (last_, length_) = lastAndLength tokens
+  (last_, length_) <- progress "Lexing..." (lastAndLength tokens)
   putStrLn (show length_ ++ " tokens, last was " ++ show last_)
