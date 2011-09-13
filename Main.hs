4fd3fe9e102cfb130a83650235427b3bdadc52dd
Nick Smallbone <nicsma@chalmers.se>
2011-09-13 10:33:04 +0200

    accept several command-line arguments

--- Main.hs
+++ Main.hs
@@ -11,22 +11,24 @@ import Text.PrettyPrint.HughesPJ
 import Clausify
 import Flags
 import qualified Data.ByteString.Char8 as BS
+import Control.Monad
 
 main = do
-  [arg] <- getArgs
+  args <- getArgs
   fl <- getFlags Equinox
-  res <- parseProblem arg
-  case res of
-    Left err -> putStrLn err
-    Right p -> do
-      -- let ?monotone = const Infinite
-      --     ?size = const Infinite
-      -- in putStrLn (prettyShow (infer p))
-      putStrLn $ "ok, " ++ show (length (open p)) ++ " clauses"
-      let ?flags = fl
-      let cs = close (clausify p) (\(cs, _) -> return [ Input (BS.pack "foo") Axiom c | c <- cs ])
-      putStrLn $ "ok, " ++ show (length (open cs)) ++ "clauses"
-      -- putStrLn (render (prettyProblem "tff" Normal p))
-      -- putStrLn (render (prettyProblem "tff" Normal cs))
-      -- putStrLn (render (prettyProblem "tff" Chatty p))
-      -- putStrLn (render (prettyProblem "tff" Chatty cs))
+  forM args $ \arg -> do
+    res <- parseProblem arg
+    case res of
+      Left err -> putStrLn err
+      Right p -> do
+        -- let ?monotone = const Infinite
+        --     ?size = const Infinite
+        -- in putStrLn (prettyShow (infer p))
+        putStrLn $ "ok, " ++ show (length (open p)) ++ " clauses"
+        let ?flags = fl
+        let cs = close (clausify p) (\(cs, _) -> return [ Input (BS.pack "foo") Axiom c | c <- cs ])
+        putStrLn $ "ok, " ++ show (length (open cs)) ++ "clauses"
+        -- putStrLn (render (prettyProblem "tff" Normal p))
+        -- putStrLn (render (prettyProblem "tff" Normal cs))
+        -- putStrLn (render (prettyProblem "tff" Chatty p))
+        -- putStrLn (render (prettyProblem "tff" Chatty cs))
