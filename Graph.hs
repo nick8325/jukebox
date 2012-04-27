836a51f1f68235ba9f62d2d06872237c0ad0485c
Koen Claessen <koen@chalmers.se>
2012-04-27 14:45:02 +0200

    added shortest path finding to Graph.hs

--- Graph.hs
+++ Graph.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Graph where
 
 import Data.Map as M
 import Data.Set as S
+import Data.Maybe
+import Data.List
+
+import Test.QuickCheck
+import Test.QuickCheck.Poly
+import Test.QuickCheck.All
 
 type Graph a = Map a [a]
 
 build :: Ord a => [(a,a)] -> Graph a
-build xys = M.fromListWith (++) [ (x,[y]) | (x,y) <- xys ]
+build xys = M.map S.toList (M.fromListWith S.union [ (x,S.singleton y) | (x,y) <- xys ])
 
 build2 :: Ord a => [(a,a)] -> Graph a
 build2 xys = build (xys ++ [ (y,x) | (x,y) <- xys ])
@@ -35,3 +42,48 @@ classes g = gather S.empty xs
            Nothing -> []
            Just ys -> ys
 
+path :: Ord a => Graph a -> a -> a -> Maybe [a]
+path g x y = find S.empty [(x,[])] [] y
+ where
+  find seen [] [] y =
+    Nothing
+  
+  find seen [] qs y =
+    find seen (reverse qs) [] y
+  
+  find seen ((x,p):ps) qs y
+    | x `S.member` seen = find seen ps qs y
+    | x == y            = Just (reverse (x:p))
+    | otherwise         = find (S.insert x seen) ps ([ (y,x:p) | Just ys <- [M.lookup x g], y <- ys ] ++ qs) y
+
+-- testing
+
+data Node = Node Int deriving ( Eq, Ord )
+data GraphA = Graph (Graph Node) deriving ( Show )
+
+instance Show Node where
+  show (Node n) = show n
+
+instance Arbitrary Node where
+  arbitrary = Node `fmap` choose (1,30)
+
+instance Arbitrary GraphA where
+  arbitrary = (Graph . build2) `fmap` arbitrary
+  shrink (Graph g) = [ Graph (build2 xs') | xs' <- shrink (edges g) ]
+   where
+    edges g = nub [ (x,y) | (x,ys) <- M.toList g, y <- ys, x <= y ]
+
+prop_Complete (Graph g) =
+  let cs = classes g
+      ns = nodes g   in
+    ns == S.fromList (concat cs)
+
+prop_BigEnough (Graph g) =
+  let cs = classes g in
+    all isJust [ path g x y | c <- cs, x <- c, y <- c ]
+
+prop_SmallEnough (Graph g) =
+  let cs = classes g in
+    all isNothing [ path g x y | c1 <- cs, c2 <- cs, c1 < c2, x <- c1, y <- c2 ]
+
+testAll = $(quickCheckAll)
