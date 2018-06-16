cef0bed07da53dc7155228a7b8f5791546fba268
Nick Smallbone <nicsma@chalmers.se>
2018-06-16 18:06:52 +0200

    Add uncnf tool for going from CNF to FOF

--- executable/Main.hs
+++ executable/Main.hs
@@ -49,7 +49,7 @@ data Tool =
     pipeline :: OptionParser (IO ()) }
 
 tools = external ++ internal
-external = [fof, cnf, smt, monotonox, hornToUnit]
+external = [fof, cnf, uncnf, smt, monotonox, hornToUnit]
 internal = [guessmodel, parse]
 
 fof =
@@ -81,6 +81,12 @@ cnf =
        clausifyBox =>>=
        oneConjectureBox =>>=
        printClausesBox)
+
+uncnf =
+  Tool "uncnf" "Introduce explicit quantification into a problem" $
+    forAllFilesBox <*>
+      (readProblemBox =>>=
+       printProblemBox)
 
 guessmodel =
   Tool "guessmodel" "Guess an infinite model (internal use)" $
